0830a98d2c6dc2aa3ed39ba5eca210f5afc6f04f
osamntn <62132308+osamntn@users.noreply.github.com>
2020-03-13 18:18:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+setTimeout(function() {
+    if (document.getElementsByTagName("html")[0].classList.contains('wf-active') != true) {
+        document.getElementsByTagName("html")[0].classList.add('loading-delay');
+    }
+}, 1500);
