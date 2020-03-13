e91a8105abb6294b6400c6081d446916f095c218
osamntn <62132308+osamntn@users.noreply.github.com>
2020-03-13 18:15:39 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-setTimeout(function() {
-    if (document.getElementsByTagName("html")[0].classList.contains('wf-active') != true) {
-        document.getElementsByTagName("html")[0].classList.add('loading-delay');
-    }
-}, 1500);
